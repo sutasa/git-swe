56735ce32e27f88b9150e3c5bfa70cba060f6783
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2017-09-14 08:31:00 +0700

    print hello n'fern

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello P'ping");
+		
+		System.out.println("Hello N'fern");
 	}
 }
