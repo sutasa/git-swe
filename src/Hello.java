e840596f7147d4b94931440867406c41ac3c2554
NEXT Speed <NEXT Speed@aBaCus>
2017-09-14 08:48:22 +0700

    eiei

--- src/Hello.java
+++ src/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
 		System.out.println("Hello P'ping");
 		System.out.println("Hello p'ping2");
 		System.out.println("Hello N'fern");
+		System.out.println("Hello LIF NAJA");
 	}
 }
