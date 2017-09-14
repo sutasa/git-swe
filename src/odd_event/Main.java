f87a58c2e802ca2c142fc29d45b4d19bf287a6a6
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2017-09-14 09:47:06 +0700

    creat main

--- src/odd_event/Main.java
+++ src/odd_event/Main.java
@@ -1,10 +1,16 @@
 package odd_event;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		
+		Checkeven sum = new Checkeven(num);
+		System.out.println(sum.output);	
 	}
-
 }
