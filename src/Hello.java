e1761ae4313740fa275e98d4eecbadd93887d0b3
DELL-PC\DELL <fernsutasa@gmail.com>
2017-09-14 08:37:12 +0700

    editconfic

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello P'ping");
-		
+		System.out.println("Hello p'ping2");
 		System.out.println("Hello N'fern");
 	}
 }
