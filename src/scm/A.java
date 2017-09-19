e592c74bc03a8e06f8099908523f0d312e64bc8d
MSI\Poon <poon1412@gmail.com>
2017-09-19 10:31:57 +0700

    Class

--- src/scm/A.java
+++ src/scm/A.java
@@ -1,5 +1,22 @@
 package scm;
 
 public class A {
-
+	double sarary;
+	double sale;
+	double result;
+	public A(double sarary,double sale) {
+		this.sarary=sarary;
+		this.sale =sale;
+	}
+	public double find() {
+		if(this.sale>50000) {
+			this.result=(this.sale*(0.03))+this.sarary;
+		}else if(this.sale>25000) {
+			this.result=(this.sale*(0.02))+this.sarary;
+		}else
+		{
+			this.result=(this.sale*(0.01))+this.sarary;
+		}
+		return this.result;
+	}
 }
